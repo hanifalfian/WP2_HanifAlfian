4ca5b9619e8363d5eb5c9ce0dd52de0e8ec25bf8
hanifalfian <hanifalfian26@gmail.com>
2023-12-09 14:20:21 +0700

    P01-P10

---
+++ application/controllers/Contoh4.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan Diri</h1>";
+        echo "Nama : Hanif alfian <br>";
+        echo "Nim  : 12220230 <br>";
+        echo "Kelas: 12.3A.05 <br>";
+        echo "TTL  : Bekasi, 10 mei 2003 <br>";
+        echo "Hobi : Bermain game"; 
+        //contoh 4.php//
+    }
+}
